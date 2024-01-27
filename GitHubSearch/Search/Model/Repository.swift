86c748de78e434569a424dd65ffabcf8b2cf3df8
Soma Limbek <soma.limbek@lastpass.com>
2024-01-27 13:27:44 +0100

    add properties to Repository struct

--- GitHubSearch/Search/Model/Repository.swift
+++ GitHubSearch/Search/Model/Repository.swift
@@ -7,6 +7,12 @@
 
 import Foundation
 
-struct Repository {
-
+struct Repository: Identifiable {
+    let id: Int
+    let avatarURL: String
+    let owner: String
+    let name: String
+    let description: String
+    let stars: Int
+    let language: String
 }
