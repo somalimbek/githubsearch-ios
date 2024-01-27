900ce997eb13c088c128a7335d3b34fbcc62460f
Soma Limbek <soma.limbek@lastpass.com>
2024-01-27 11:23:45 +0100

    Initial Commit

---
+++ GitHubSearch/GitHubSearchApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitHubSearchApp.swift
+//  GitHubSearch
+//
+//  Created by Soma Limbek on 2024. 01. 27..
+//
+
+import SwiftUI
+
+@main
+struct GitHubSearchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
