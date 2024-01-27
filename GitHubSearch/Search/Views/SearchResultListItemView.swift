08e2859d0e807d54e02530603d60a8d527619a4e
Soma Limbek <soma.limbek@lastpass.com>
2024-01-27 13:27:52 +0100

    implement list item view

--- GitHubSearch/Search/Views/SearchResultListItemView.swift
+++ GitHubSearch/Search/Views/SearchResultListItemView.swift
@@ -8,11 +8,48 @@
 import SwiftUI
 
 struct SearchResultListItemView: View {
+
+    let repository: Repository
+
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack(alignment: .leading, spacing: 8) {
+            HStack(spacing: 8) {
+                AsyncImage(url: URL(string: repository.avatarURL)) { image in
+                    image.resizable()
+                } placeholder: {
+                    ProgressView()
+                }
+
+                Text(repository.owner)
+            }
+
+            Text(repository.name)
+
+            Text(repository.description)
+
+            HStack(spacing: 24) {
+                HStack {
+                    Image(systemName: "star")
+
+                    Text("\(repository.stars)")
+                }
+
+                Text(repository.language)
+            }
+        }
     }
 }
 
 #Preview {
-    SearchResultListItemView()
+    SearchResultListItemView(
+        repository: Repository(
+            id: 1,
+            avatarURL: "https://secure.gravatar.com/avatar/e7956084e75f239de85d3a31bc172ace?d=https://a248.e.akamai.net/assets.github.com%2Fimages%2Fgravatars%2Fgravatar-user-420.png",
+            owner: "dtrupenn",
+            name: "Tetris",
+            description: "A C implementation of Tetris using Pennsim through LC4",
+            stars: 1,
+            language: "Assembly"
+        )
+    )
 }
