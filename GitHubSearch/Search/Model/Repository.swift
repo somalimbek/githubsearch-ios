4c8aca54e61f350c41a81ac565d0b7cd6773d500
Soma Limbek <soma.limbek@lastpass.com>
2024-01-28 21:22:16 +0100

    make Repository decodable

--- GitHubSearch/Search/Model/Repository.swift
+++ GitHubSearch/Search/Model/Repository.swift
@@ -7,7 +7,7 @@
 
 import Foundation
 
-struct Repository: Identifiable {
+struct Repository: Identifiable, Decodable {
     let id: Int
     let avatarURL: String
     let owner: String
@@ -16,4 +16,33 @@ struct Repository: Identifiable {
     let stars: Int
     let language: String
     let url: String
+
+    enum CodingKeys: String, CodingKey {
+        case id
+        case owner
+        case name
+        case description
+        case stars = "stargazers_count"
+        case language
+        case url
+    }
+
+    enum OwnerKeys: String, CodingKey {
+        case avatarURL = "avatar_url"
+        case owner = "login"
+    }
+
+    init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        let ownerContainer = try container.nestedContainer(keyedBy: OwnerKeys.self, forKey: .owner)
+
+        id = try container.decode(Int.self, forKey: .id)
+        avatarURL = try ownerContainer.decode(String.self, forKey: .avatarURL)
+        owner = try ownerContainer.decode(String.self, forKey: .owner)
+        name = try container.decode(String.self, forKey: .name)
+        description = try container.decode(String.self, forKey: .description)
+        stars = try container.decode(Int.self, forKey: .stars)
+        language = try container.decode(String.self, forKey: .language)
+        url = try container.decode(String.self, forKey: .url)
+    }
 }
