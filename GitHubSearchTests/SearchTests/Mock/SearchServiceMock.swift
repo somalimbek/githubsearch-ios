bb426dd4708c3d5db48a149c194fc004459ff961
Soma Limbek <soma.limbek@lastpass.com>
2024-01-28 18:27:26 +0100

    PASS - implement SearchServiceMock

---
+++ GitHubSearchTests/SearchTests/Mock/SearchServiceMock.swift
@@ -0,0 +1,33 @@
+//
+//  SearchServiceMock.swift
+//  GitHubSearchTests
+//
+//  Created by Soma Limbek on 2024. 01. 28..
+//
+
+import Foundation
+@testable import GitHubSearch
+
+final class SearchServiceMock {
+    var didCallSearchRepositories = false
+    var searchRepositoriesReceivedParameter: String?
+    var repositoryMock = Repository(
+        id: 1,
+        avatarURL: "https://secure.gravatar.com/avatar/e7956084e75f239de85d3a31bc172ace?d=https://a248.e.akamai.net/assets.github.com%2Fimages%2Fgravatars%2Fgravatar-user-420.png",
+        owner: "dtrupenn",
+        name: "Tetris",
+        description: "A C implementation of Tetris using Pennsim through LC4",
+        stars: 1,
+        language: "Assembly",
+        url: "https://github.com/dtrupenn/Tetris"
+    )
+}
+
+extension SearchServiceMock: SearchServiceProtocol {
+    func searchRepositories(query: String) -> [Repository] {
+        didCallSearchRepositories = true
+        searchRepositoriesReceivedParameter = query
+
+        return [repositoryMock]
+    }
+}
